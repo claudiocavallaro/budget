7f377795d6f7845196ce0dfd32bc410802e88a0b
claudio cavallaro <claudio.cavallaro91@gmail.com>
2021-04-25 10:22:19 +0200

    Some comments

--- budget/src/main/java/com/example/testdb/demodb/api/PagamentoController.java
+++ budget/src/main/java/com/example/testdb/demodb/api/PagamentoController.java
@@ -12,16 +12,19 @@ public class PagamentoController {
     @Autowired
     private PagamentiService service;
 
+    //Get all payment
     @GetMapping("/get")
     public String selectMetod(){
         return service.get();
     }
 
+    //Get Month payment
     @GetMapping("/getMonth")
     public String selectByMonth(@RequestParam String month){
         return service.getMonth(month);
     }
 
+    //Add Payment
     @PostMapping("/add")
     public String add(@RequestBody PagamentoEffettuato pagamentoEffettuato){
         return service.insert(pagamentoEffettuato);
